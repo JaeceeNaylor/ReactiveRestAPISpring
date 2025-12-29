f3171e97f9aa13673dfeef22770dbb7f61a7e10c
tylerbb812 <tylerbb812@yahoo.com>
2025-12-29 00:39:17 -0700

    semgrep step 8

--- solution/step8/Product.java
+++ solution/step8/Product.java
@@ -2,6 +2,7 @@ package com.example.products.domain;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
+import java.math.BigDecimal;
 
 @Table("products")
 public class Product {
